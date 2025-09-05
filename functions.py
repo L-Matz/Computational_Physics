b9c2fe24ca67f6a31ae49582c2337093179085a4
LukeM <ldmatzne@syr.edu>
2025-09-04 20:05:50 -0400

    Updated functions.py to correct some bugs and make clearer coding

--- functions.py
+++ functions.py
@@ -2,13 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import g as grav_const
 
+#Define constants: gravity. Was made negative for negative y direction
+grav_const = -9.8
+#m = 1000
+
 def initial_conditions():
 
-    pos_x, pos_y = input("Enter comma-separated initial position: ")
-    vel_x, vel_y = input("Enter comma-separated initial velocity: ")
+    pos_x, pos_y = input("Enter comma-separated initial position (m): ")
+    vel_x, vel_y = input("Enter comma-separated initial velocity (m/s): ")
 
     C_d = input("Enter Drag Coefficient: ")
-    m = input("Enter mass: ")
+    m = input("Enter mass (kg): ")
     dt = input("Enter time-step (s): ")
 
     pos_vec = {"pos_x":pos_x, "pos_y": pos_y}
@@ -19,18 +23,17 @@ def initial_conditions():
 #pos_vec, vel_vec, C_d, m = initial_conditions() !!!!include this code to query for init conditions
 #force_vec = {"force_x":0, "force_y":0}
 
-def force_vector(pos_vec, vel_vec, force_vec):
+def force_vector(pos_vec, vel_vec):
 
     drag_mag = C_d*(vel_vec["vel_x"]**2 + vel_vec["vel_y"]**2)
     grav_mag = m * grav_const
 
-    drag_angle = np.arctan2(vel_vec["vel_y"],vel_vec["vel_y"])
+    drag_angle = np.arctan2(vel_vec["vel_y"],vel_vec["vel_x"])
     
     Fd_x = -drag_mag * np.sin(drag_angle)
     Fd_y = -drag_mag * np.cos(drag_angle)
     Fg_y = -grav_mag
-
-    force_vec["force_x"], force_vec["force_y"] = Fd_x, Fg_y + Fd_y
+    force_vec = { "force_x":(Fd_x), "force_y":(Fg_y + Fd_y)}
     
     return force_vec
 
