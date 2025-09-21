104a85b7bb55979f6484a75c2bece0350a372ee1
LukeM <ldmatzne@syr.edu>
2025-09-21 17:55:18 -0400

    Adjusted code with black to format it correctly.

--- run_euler.py
+++ run_euler.py
@@ -1,4 +1,4 @@
-#This is a file for running the main loop, so I will need to initial variables, and call in the python functions in functions.py. 
+# This is a file for running the main loop, so I will need to initial variables, and call in the python functions in functions.py.
 import functions as func
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,40 +9,43 @@ PE, KE, TE = [], [], []
 
 vel_now_vec, dis_now_vec, k, m, tmax, dt = func.initial_conditions_horizontal()
 dis_x_euler, vel_x_euler = [], []
-#Recording initial conditions
+# Recording initial conditions
 dis_x_euler.append(dis_now_vec["dis_x"])
 vel_x_euler.append(vel_now_vec["vel_x"])
-KE.append(func.calculate_KE(vel_now_vec,m))
-PE.append(func.calculate_PE(dis_now_vec,m,k))
+KE.append(func.calculate_KE(vel_now_vec, m))
+PE.append(func.calculate_PE(dis_now_vec, m, k))
 TE.append(KE[-1] + PE[-1])
 
+#Determining the amount of counts to run the simulation.
 countmax = tmax / dt
 count = 0
-while(count < countmax):
+#Running iteration for each point
+while count < countmax:
     time_list.append(time_list[-1] + dt)
-    force_now = func.update_force_no_gravity(dis_now_vec,k)
-    vel_next_vec = func.update_vel_euler(force_now,vel_now_vec,m,dt)
-    dis_next_vec = func.update_dis_euler(dis_now_vec,vel_now_vec,dt)
-    
+    force_now = func.update_force_no_gravity(dis_now_vec, k)
+    vel_next_vec = func.update_vel_euler(force_now, vel_now_vec, m, dt)
+    dis_next_vec = func.update_dis_euler(dis_now_vec, vel_now_vec, dt)
+
     dis_x_euler.append(dis_next_vec["dis_x"])
     vel_x_euler.append(vel_next_vec["vel_x"])
-    KE.append(func.calculate_KE(vel_next_vec,m))
-    PE.append(func.calculate_PE(dis_next_vec,m,k))
+    KE.append(func.calculate_KE(vel_next_vec, m))
+    PE.append(func.calculate_PE(dis_next_vec, m, k))
     TE.append(KE[-1] + PE[-1])
-    
+
     dis_now_vec = dis_next_vec
     vel_now_vec = vel_next_vec
-        
+
     count = count + 1
-#Making graph
-fig, axs = plt.subplots(ncols = 2,layout='constrained')
-axs[0].plot(time_list,dis_x_euler,label='displacement')
-axs[0].set_xlabel('time [s]')
+# Making graph
+fig, axs = plt.subplots(ncols=2, layout="constrained")
+axs[0].plot(time_list, dis_x_euler, label="displacement")
+axs[0].set_xlabel("time [s]")
 axs[0].set_ylabel("X-displacement [m]")
+axs[0].set_title("Euler Explicit Method")
 
 
-axs[1].plot(time_list,TE,label='energy')
-axs[1].set_xlabel('time [s]')
-axs[1].set_ylabel('Energy [J]')
-plt.savefig("graphs_displacement_energy_euler_{tmax}_{dt}.pdf")
+axs[1].plot(time_list, TE, label="energy")
+axs[1].set_xlabel("time [s]")
+axs[1].set_ylabel("Total Energy [J]")
+plt.savefig(f"graphs_displacement_energy_euler_dt:{dt}.pdf")
 plt.close()
--- run_midpoint.py
+++ run_midpoint.py
@@ -1,4 +1,4 @@
-#This is a file for running the main loop, so I will need to initial variables, and call in the python functions in functions.py. 
+# This is a file for running the main loop, so I will need to initial variables, and call in the python functions in functions.py.
 import functions as func
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,46 +9,49 @@ PE, KE, TE = [], [], []
 
 vel_now_vec, dis_now_vec, k, m, tmax, dt = func.initial_conditions_horizontal()
 dis_x_midpoint, vel_x_midpoint = [], []
-#Recording initial conditions
+# Recording initial conditions
 dis_x_midpoint.append(dis_now_vec["dis_x"])
 vel_x_midpoint.append(vel_now_vec["vel_x"])
-KE.append(func.calculate_KE(vel_now_vec,m))
-PE.append(func.calculate_PE(dis_now_vec,m,k))
+KE.append(func.calculate_KE(vel_now_vec, m))
+PE.append(func.calculate_PE(dis_now_vec, m, k))
 TE.append(KE[-1] + PE[-1])
 
+#Determining the amount of counts to run the simulation
 countmax = tmax / dt
 count = 0
-while(count < countmax):
+#Iterating over the simulation.
+while count < countmax:
     time_list.append(time_list[-1] + dt)
-    #Find the velocity half way point between this step and the next step to get next dislpacement
-    force_now_vec = func.update_force_no_gravity(dis_now_vec,k)
-    vel_half_vec = func.update_vel_euler(force_now_vec,vel_now_vec,m,(dt/2))
-    dis_next_vec = func.update_dis_euler(dis_now_vec,vel_half_vec,dt)
-    
-    #Find the the half way point in displacement to get the half way point in acceleration, as to get next velocity step.
-    dis_half_vec = func.update_dis_euler(dis_now_vec,vel_now_vec,(dt/2))
-    force_half_vec = func.update_force_no_gravity(dis_half_vec,k)
-    vel_next_vec = func.update_vel_euler(force_half_vec,vel_now_vec,m,dt)
-    
+    # Find the velocity half way point between this step and the next step to get next dislpacement
+    force_now_vec = func.update_force_no_gravity(dis_now_vec, k)
+    vel_half_vec = func.update_vel_euler(force_now_vec, vel_now_vec, m, (dt / 2))
+    dis_next_vec = func.update_dis_euler(dis_now_vec, vel_half_vec, dt)
+
+    # Find the the half way point in displacement to get the half way point in acceleration, as to get next velocity step.
+    dis_half_vec = func.update_dis_euler(dis_now_vec, vel_now_vec, (dt / 2))
+    force_half_vec = func.update_force_no_gravity(dis_half_vec, k)
+    vel_next_vec = func.update_vel_euler(force_half_vec, vel_now_vec, m, dt)
+
     dis_x_midpoint.append(dis_next_vec["dis_x"])
     vel_x_midpoint.append(vel_next_vec["vel_x"])
-    KE.append(func.calculate_KE(vel_next_vec,m))
-    PE.append(func.calculate_PE(dis_next_vec,m,k))
+    KE.append(func.calculate_KE(vel_next_vec, m))
+    PE.append(func.calculate_PE(dis_next_vec, m, k))
     TE.append(KE[-1] + PE[-1])
-    
+
     dis_now_vec = dis_next_vec
     vel_now_vec = vel_next_vec
-        
+
     count = count + 1
-#Making graph
-fig, axs = plt.subplots(ncols = 2,layout='constrained')
-axs[0].plot(time_list,dis_x_midpoint,label='displacement')
-axs[0].set_xlabel('time [s]')
+# Making graph
+fig, axs = plt.subplots(ncols=2, layout="constrained")
+axs[0].plot(time_list, dis_x_midpoint, label="displacement")
+axs[0].set_xlabel("time [s]")
 axs[0].set_ylabel("X-displacement [m]")
+axs[0].set_title("Midpoint Method")
 
 
-axs[1].plot(time_list,TE,label='energy')
-axs[1].set_xlabel('time [s]')
-axs[1].set_ylabel('Energy [J]')
-plt.savefig("graphs_displacement_energy_midpoint_test.pdf")
+axs[1].plot(time_list, TE, label="energy")
+axs[1].set_xlabel("time [s]")
+axs[1].set_ylabel("Total Energy [J]")
+plt.savefig(f"graphs_displacement_energy_midpoint_dt:{dt}.pdf")
 plt.close()
